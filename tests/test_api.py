468afd988e3dc895551b18f68eb36612b67b10bf
VanderSant <vanderson.santos@enline-transmission.com>
2023-08-03 11:04:30 -0300

    🐛 test pre commit

--- tests/test_api.py
+++ tests/test_api.py
@@ -16,3 +16,11 @@ def test_status(client):
     res = json.loads(response.data.decode("utf-8"))
     print(res)
     assert res["Status"] == "heart beating steady and strong"
+
+
+def test_status_2(client):
+    """Status route test."""
+    response = client.get("/healthcheck")
+    res = json.loads(response.data.decode("utf-8"))
+    print(res)
+    assert res["Status"] == "heart beating steady and strong"
